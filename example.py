5ac72bab70462efee2491ef6d63a26bd0defcb34
Zijie Wu <zijiewu@udel.edu>
2019-11-06 00:57:43 -0500

    get example.py ready

--- example.py
+++ example.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#import parmed as pmd
-#from foyer import Forcefield
 import mbuild as mb
 from mbuild_ONA.mbuild_ONA import ONA_box
-#Create topology in mbuild
 import py3Dmol
 import mbuild_ONA
 test_box = ONA_box(['AAC','GTAC'],[1,1,2])
@@ -16,12 +7,3 @@ test_box.write_lammps('test.lammps')
 
 
 
-# In[3]:
-
-
-##Create forcefield in foyer
-#cgff = Forcefield(forcefield_files='ONA.xml')
-##apply it to the mbuild structure
-#test_box_typed =cgff.apply(test_box,assert_dihedral_params=False)
-##Output a LAMMPS data file
-#mb.formats.lammpsdata.write_lammpsdata(test_box_typed,'test_output.lammps',atom_style='molecular')
