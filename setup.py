0d1152aaacb46bb064995272306abf00e2e17f82
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-11-12 11:20:44 -0500

    Change entrypoint to DNA class

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
 
 setup(
-      name="mbuild_ona",
-      install_requires="mbuild",
-#      entry_points={
-#                    "mbuild.plugins":[ "ona_box = mbuild_ona.mbuild_ona:ona_box"
-#                        ]
-#                    },
-#                    py_modules=["mbuild_ona"],
+    name="mbuild_ona",
+    install_requires="mbuild",
+    entry_points={"mbuild.plugins": ["DNA = mbuild_ona.mbuild_ona:DNA"]},
+    py_modules=["mbuild_ona"],
 )
