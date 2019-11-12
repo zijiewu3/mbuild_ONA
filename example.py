7d8d6a4e529d4d467cc7926201d70ea6b1199f34
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-11-12 11:16:17 -0500

    Move example to examples directory

--- example.py
+++
@@ -1,8 +0,0 @@
-import mbuild as mb
-from mbuild_ONA.mbuild_ONA import ONA_box
-import py3Dmol
-import mbuild_ONA
-test_box = ONA_box(['AAC','GTAC'],[1,1,2])
-test_box.write_lammps('test.lammps')
-test_box.create_lammps_input_script(sim_name='test.lammps')
-
---
+++ examples/basic_example.py
@@ -0,0 +1,7 @@
+import mbuild as mb
+test_box = mb.recipes.ONA_box(
+        ['AATT','GGCC'],
+        [2,1,1],
+)
+test_box.write_lammps('test_from_mb.lammps')
+test_box.create_lammps_input_script(sim_name='test_from_mb.lammps')
